6caad3d6ead233bb34743d9430afdc1fe61097c0
Aloshyn Olexandr <aleshoff@gmail.com>
2024-09-17 10:42:16 +0300

    Addesd nginx endpoint

--- index.mjs
+++ index.mjs
@@ -5,9 +5,16 @@ const app = express()
 const PORT = 3000
 
 app.get("/", (req, res) => {
-  const helloMessage = `<h1>VERSION 2: Hello from the ${os.hostname()}</h1>`
+  const helloMessage = `<h1>Hello from the ${os.hostname()}</h1>`
   console.log(helloMessage)
   res.send(helloMessage)
+})
+
+app.get("/nginx", async (req, res) => {
+  const url = 'http://nginx'
+  const response = await fetch(url);
+  const body = await response.text();
+  res.send(body)
 })
 
 app.listen(PORT, () => {
